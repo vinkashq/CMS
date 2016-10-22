e45cf29709d32ea5911d97117068f16b3e4e6ae3
Vinoth Kannan <vinothkannan@vinkas.com>
2016-10-23 01:02:58 +0530

    new: login & register buttons added

--- resources/views/sections/header.blade.php
+++ resources/views/sections/header.blade.php
@@ -7,7 +7,16 @@
         </a>
       </div>
       <div class="panel clearfix">
-        <span></span>
+        <span>
+          @if (Route::has('login'))
+              @if (Auth::check())
+                  <a class="widget-button btn-primary btn-sm" href="{{ url('/home') }}">Home</a>
+              @else
+                  <a class="widget-button btn-primary btn-sm sign-up-button" href="{{ url('/register') }}">Register</a>
+                  <a class="widget-button btn-primary btn-sm login-button" href="{{ url('/login') }}">Login</a>
+              @endif
+          @endif
+        </span>
         <ul role="navigation" class="icons clearfix">
           <li class="header-dropdown-toggle">
             <a data-auto-route="true" title="go to another page" aria-label="go to another page" id="toggle-hamburger-menu" class="icon"><i class="fa fa-bars" aria-hidden="true"></i></a>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,17 +2,6 @@
 
 @section('content')
         <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @if (Auth::check())
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ url('/login') }}">Login</a>
-                        <a href="{{ url('/register') }}">Register</a>
-                    @endif
-                </div>
-            @endif
-
             <div class="content">
                 <div class="title m-b-md">
                     Vinkas CMS
