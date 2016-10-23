8e96db33df797e04e2a0e3c96ef87e76ad2ef2c5
Vinoth Kannan <vinothkannan@vinkas.com>
2016-10-23 23:28:46 +0530

    new: footer blade section added

--- resources/views/sections/footer.blade.php
+++ resources/views/sections/footer.blade.php
@@ -1,2 +1,17 @@
 <footer class="v-footer clearfix">
+  <div class="container">
+  </div>
+  <div class="footer-bottom">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6">
+        </div>
+        <div class="col-md-6">
+          <div class="powered-by text-right">
+            Powered by <a href="https://github.com/vinkas0/CMS">Vinkas CMS</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </footer>
