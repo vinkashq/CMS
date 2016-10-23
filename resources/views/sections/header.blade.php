92fc33887a1ee68cf0c5fe87629bf3a7737fd502
Vinoth Kannan <vinothkannan@vinkas.com>
2016-10-23 11:41:55 +0530

    new: text logo added by default

--- resources/views/sections/header.blade.php
+++ resources/views/sections/header.blade.php
@@ -2,8 +2,8 @@
   <div class="wrap">
     <div class="contents clearfix">
       <div class="title">
-        <a href="/" data-auto-route="true">
-          <img src="" alt="" id="site-logo" class="logo-big">
+        <a href="/">
+          <h2 id="site-text-logo" class="text-logo">{{ config('app.name') }}</h2>
         </a>
       </div>
       <div class="panel clearfix">
@@ -21,13 +21,17 @@
           <li class="header-dropdown-toggle">
             <a data-auto-route="true" title="go to another page" aria-label="go to another page" id="toggle-hamburger-menu" class="icon"><i class="fa fa-bars" aria-hidden="true"></i></a>
           </li>
-          <li id="current-user" class="header-dropdown-toggle current-user">
-            <a href="" data-auto-route="true" class="icon">
-              <div>
-                <img alt="" width="32" height="32" src="" title="" class="avatar">
-              </div>
-            </a>
-          </li>
+          @if (Route::has('login'))
+              @if (Auth::check())
+                <li id="current-user" class="header-dropdown-toggle current-user">
+                  <a href="" data-auto-route="true" class="icon">
+                    <div>
+                      <img alt="" width="32" height="32" src="" title="" class="avatar">
+                    </div>
+                  </a>
+                </li>
+              @endif
+          @endif
         </ul>
       </div>
     </div>
---
+++ resources/views/skeleton.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>{{ config('app.name') }}</title>
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <meta name="generator" content="">
+  <link rel="icon" type="image/png" href="">
+  <link rel="apple-touch-icon" type="image/png" href="">
+  <link rel="icon" sizes="144x144" href="">
+  <meta name="theme-color" content="#ffffff">
+  <meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0, user-scalable=yes">
+
+  <!-- Styles -->
+  <link href="/css/app.css" rel="stylesheet">
+
+  <!-- CSRF Token -->
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+  <meta name="fragment" content="!">
+
+  <!-- Scripts -->
+  <script>
+  window.Laravel = <?php echo json_encode([
+    'csrfToken' => csrf_token(),
+  ]); ?>
+  </script>
+</head>
+<body class="docked">
+  @yield('layout')
+  <div id="offscreen-content">
+  </div>
+  <!-- Scripts -->
+  <script src="/js/app.js"></script>
+</body>
+</html>
